bc45c33b2efb754691947377f76cceae0d5e8772
rishyym0927 <rishyym0927@gmail.com>
2024-10-03 15:15:45 +0530

    Fixes #25 Added The Error Page

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const userRouter = require("./routes/user.js");
 /* if you want to run the project and do not have .env files configured , please uncomment the next
 two commented codes and comment down this code   await mongoose.connect(dbUrl); You project will run succesfully*/
 
-//const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const dbUrl =process.env.ATLASDB_URL;
 main()
   .then(() => {
@@ -28,8 +28,8 @@ main()
     console.log(err);
   });
 async function main(){
-  //await mongoose.connect(MONGO_URL);
-  await mongoose.connect(dbUrl);
+  await mongoose.connect(MONGO_URL);
+  // await mongoose.connect(dbUrl);
 }
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
